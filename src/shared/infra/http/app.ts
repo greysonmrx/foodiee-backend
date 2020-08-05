c82ba3649a1a429614395cbf5081c4e8a6feadc3
Greyson Mascarenhas <greysonmrx@gmail.com>
2020-08-04 21:09:59 -0300

    feat: add initial setup
    create the base structure of the project

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,53 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+
+import express, { Express, Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+
+import createConnection from '@shared/infra/typeorm';
+import AppError from '@shared/errors/AppError';
+import routes from './routes';
+
+class App {
+  public server: Express;
+
+  constructor() {
+    createConnection();
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+    this.exceptionHandler();
+  }
+
+  private middlewares(): void {
+    this.server.use(cors());
+    this.server.use(express.json());
+  }
+
+  private routes(): void {
+    this.server.use(routes);
+  }
+
+  private exceptionHandler(): void {
+    this.server.use((err: Error, request: Request, response: Response, _next: NextFunction) => {
+      if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+          status: 'error',
+          message: err.message,
+        });
+      }
+
+      // eslint-disable-next-line
+      console.log(err);
+
+      return response.status(500).json({
+        status: 'error',
+        message: 'Erro interno do servidor.',
+      });
+    });
+  }
+}
+
+export default new App().server;

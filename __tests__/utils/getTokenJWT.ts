6700347055bde834839ca4187e3808e3156b18ec
Greyson Mascarenhas <greysonmrx@gmail.com>
2020-08-06 21:56:51 -0300

    feat: create endpoint to update users
    A feature was created to update a user in the PUT:/profiles route, as well as all tests to validate
    the user's update.

--- __tests__/utils/getTokenJWT.ts
+++ __tests__/utils/getTokenJWT.ts
@@ -1,10 +1,10 @@
 import request from 'supertest';
 import app from '../../src/shared/infra/http/app';
 
-export default async function getTokenJWT(): Promise<string> {
+export default async function getTokenJWT(email: string, password: string): Promise<string> {
   const response = await request(app).post('/sessions').send({
-    email: 'fakeadmin@tenant.com.br',
-    password: '123456',
+    email,
+    password,
   });
 
   return response.body.token;
